70ee0181fcd314012e8fc74b3255a90d29071e29
ryanbatten <rmb788@mun.ca>
2021-06-05 12:28:53 -0300

    changing name from sd to calculated.sd in mean_sd function

--- R/descriptive_stats.R
+++ R/descriptive_stats.R
@@ -8,10 +8,10 @@
 #' @return returns the mean (SD) rounded to 2 decimal places
 #' @export
 mean_sd <- function(x) {
-  mean <- base::round(base::mean(x), 2)
-  sd <- base::round(base::sd(x), 2)
+  calculated.mean <- base::round(base::mean(x), 2)
+  calculated.sd <- base::round(base::sd(x), 2)
 
-  df.out <- base::paste0(mean, " (", sd, ")")
+  df.out <- base::paste0(calculated.mean, " (", calculated.sd, ")")
 
   return(df.out)
 }
