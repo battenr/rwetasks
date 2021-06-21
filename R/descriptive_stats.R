72856a961626ac9c3163ffc73978aa55493db534
ryanbatten <rmb788@mun.ca>
2021-06-20 22:23:15 -0300

    adding function for calculating median and IQR, updating mean_sd function to work, using stats::sd instead of base::sd

--- R/descriptive_stats.R
+++ R/descriptive_stats.R
@@ -9,9 +9,24 @@
 #' @export
 mean_sd <- function(x) {
   calculated.mean <- base::round(base::mean(x), 2)
-  calculated.sd <- base::round(base::sd(x), 2)
+  calculated.sd <- base::round(stats::sd(x), 2)
 
   df.out <- base::paste0(calculated.mean, " (", calculated.sd, ")")
+
+  return(df.out)
+}
+
+#' Calculate Median and Interquartile Range (IQR)
+#'
+#' @param x a numeric vector x (ie, df$x)
+#'
+#' @return returns the mean (SD) rounded to 2 decimal places
+#' @export
+median_iqr <- function(x) {
+  calculated.median <- base::round(stats::median(x), 2)
+  calculated.IQR <- base::round(stats::IQR(x), 2)
+
+  df.out <- base::paste0(calculated.median, " (", calculated.IQR, ")")
 
   return(df.out)
 }
